83efa00690663e55e820f0b3fc578de80c3a655e
Andrey Teplyashin <platinumthinker@gmail.com>
2015-12-05 14:39:48 +0600

    Resolve 4.1

---
+++ day_4/src/resolve.erl
@@ -0,0 +1,44 @@
+-module(resolve).
+-export([main/1]).
+
+-mode(compile).
+-include_lib("eunit/include/eunit.hrl").
+
+-define(OUT(F,A), io:format(F ++ "\n",A)).
+
+main(_Args) ->
+    Res = resolve(input()),
+    ?OUT("Out: ~p", [Res]).
+
+input() ->
+    <<"ckczppom">>.
+
+resolve(Input) ->
+    step(Input, 0, hash(Input, 0)).
+
+step(_, Postfix, true) -> Postfix;
+step(Input, Postfix, _) ->
+    Inc = Postfix + 1,
+    step(Input, Inc, hash(Input, Inc)).
+
+hash(Bin, Inc) ->
+    BinInc = integer_to_binary(Inc),
+    Hash = binary:part(crypto:hash(md5, <<Bin/binary, BinInc/binary>>), 0, 5),
+    R = << <<Y>> || <<X:4>> <= Hash, Y <- integer_to_list(X, 16)>>,
+    binary:match(R, <<"00000">>, [{scope,{0, 5}}]) == {0, 5}.
+
+-ifdef(TEST).
+
+hash_test() ->
+    ?assert(hash(<<"abcdef">>, 609043)),
+    ?assert(hash(<<"pqrstuv">>, 1048970)).
+
+resolve_test_() ->
+    {timeout, 8000,
+     [
+      ?_assertEqual(resolve(<<"abcdef">>), 609043),
+      ?_assertEqual(resolve(<<"pqrstuv">>), 1048970)
+     ]
+    }.
+
+-endif. %%TEST
