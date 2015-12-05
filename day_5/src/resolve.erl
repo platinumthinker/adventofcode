45cfa9e0736173641920b70b049deb9c1b53523e
Andrey Teplyashin <platinumthinker@gmail.com>
2015-12-05 16:34:19 +0600

    Resolve 5.2

--- day_5/src/resolve.erl
+++ day_5/src/resolve.erl
@@ -3,7 +3,12 @@
 
 -mode(compile).
 
--define(OUT(F,A), io:format(F ++ "\n",A)).
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-define(OUT(F,A), ?debugFmt(F ++ "\n", A)).
+-else.
+-define(OUT(F,A), io:format(F ++ "\n", A)).
+-endif.
 
 main(_Args) ->
     Res = resolve(input()),
@@ -16,39 +21,49 @@ input() ->
         Str <- binary:split(Data, [<<"\n">>], [global]) ].
 
 is_nice(String) ->
-    ?OUT("~s => ~p ~p ~p", [ String, rule1(String), rule2(String), rule3(String)] ),
-    rule3(String) andalso ( rule1(String) andalso rule2(String) ).
+    ?OUT("~s => ~p ~p", [ String, rule1(String), rule2(String)] ),
+    rule1(String) andalso rule2(String).
 
-rule1(String) ->
-    case re:run(String, "[aeiou]", [global]) of
-        {match, Capture} when length(Capture) > 2 -> true;
-        _ -> false
-    end.
+rule1([X, Y | Tail]) ->
+    case rule1(Tail, [X, Y]) of
+        true -> true;
+        _ -> rule1([Y | Tail])
+    end;
+rule1(_) -> false.
 
-rule2([X, X | _Tail]) -> true;
+rule1([_ | Tail] = String, Acc) ->
+    case string:str(String, Acc) of
+        0 -> rule1(Tail, Acc);
+        _ -> true
+    end;
+rule1(_, _) -> false.
+
+rule2([X, _, X | _Tail]) -> true;
 rule2([_ | Tail]) -> rule2(Tail);
 rule2(_) -> false.
 
-rule3([$a, $b | _Tail]) -> false;
-rule3([$c, $d | _Tail]) -> false;
-rule3([$p, $q | _Tail]) -> false;
-rule3([$x, $y | _Tail]) -> false;
-rule3([_ | Tail]) -> rule3(Tail);
-rule3(_) -> true.
 
 resolve(Input) ->
     length(lists:filter(fun is_nice/1, Input)).
 
 -ifdef(TEST).
--include_lib("eunit/include/eunit.hrl").
+
+rule1_test() ->
+    ?assert(rule1("xyxy")),
+    ?assert(rule1("aabcdefgaa")),
+    ?assert(not rule1("aaa")).
+
+rule2_test() ->
+    ?assert(rule2("xyx")),
+    ?assert(rule2("abcdefeghi")),
+    ?assert(rule2("aaa")).
 
 nice_test() ->
-    ?assert(is_nice("ugknbfddgicrmopn")),
-    ?assert(is_nice("aaa")).
+    ?assert(is_nice("qjhvhtzxzqqjkmpb")),
+    ?assert(is_nice("xxyxx")).
 
 naughty_test() ->
-    ?assert(not is_nice("jchzalrnumimnmhp")),
-    ?assert(not is_nice("haegwjzuvuyypxyu")),
-    ?assert(not is_nice("dvszwmarrgswjxmb")).
+    ?assert(not is_nice("uurcxstgmygtbstg")),
+    ?assert(not is_nice("ieodomkazucvgmuy")).
 
 -endif. %%TEST
