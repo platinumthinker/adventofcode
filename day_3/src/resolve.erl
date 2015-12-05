6962b7326345ab85cb62b47e2f9b6b53f0598eaf
Andrey Teplyashin <platinumthinker@gmail.com>
2015-12-05 12:55:22 +0600

    Resolve 3.2

--- day_3/src/resolve.erl
+++ day_3/src/resolve.erl
@@ -10,15 +10,32 @@ input() ->
     {ok, Binary} = file:read_file("input.txt"),
     binary:part(Binary, 0, byte_size(Binary) - 1).
 
-resolve(Input) -> add(Input, {0, 0}, {0, #{}}).
+resolve(Input) ->
+    step(Input, #{0 => {0, 0}, 1 => {0, 0}}, {1, #{ {0, 0} => 1 }}).
 
-add(Path, Key, {Count, Map}) when is_map(Map#{Key := 1}) ->
-    move(Path, Key, {Count, Map#{Key => 1}});
-add(Path, Key, {Count, Map}) ->
-    move(Path, Key, {Count + 1, Map#{Key => 1}}).
+add(Key, {Count, Map}) when is_map(Map#{Key := 1}) ->
+    {Count, Map#{Key => 1}};
+add(Key, {Count, Map}) ->
+    {Count + 1, Map#{Key => 1}}.
 
-move(<<$^, T/binary>>, {X, Y}, Acc) -> add(T, {X, Y + 1}, Acc);
-move(<<$v, T/binary>>, {X, Y}, Acc) -> add(T, {X, Y - 1}, Acc);
-move(<<$>, T/binary>>, {X, Y}, Acc) -> add(T, {X + 1, Y}, Acc);
-move(<<$<, T/binary>>, {X, Y}, Acc) -> add(T, {X - 1, Y}, Acc);
-move(_, _, {Count, _}) -> Count.
+step(<<Dir, T/binary>>, Keys, Acc) ->
+    Index = size(T) rem 2,
+    #{ Index := Key } = Keys,
+    NewKey = move(Dir, Key),
+    step(T, Keys#{ Index => NewKey}, add(NewKey, Acc));
+step(_, _, {Count, _}) -> Count.
+
+move($^, {X, Y}) -> {X, Y + 1};
+move($v, {X, Y}) -> {X, Y - 1};
+move($>, {X, Y}) -> {X + 1, Y};
+move($<, {X, Y}) -> {X - 1, Y}.
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+resolve_test() ->
+    ?assertEqual(resolve(<<"^v">>), 3),
+    ?assertEqual(resolve(<<"^>v<">>), 3),
+    ?assertEqual(resolve(<<"^v^v^v^v^v">>), 11).
+
+-endif. %%TEST
