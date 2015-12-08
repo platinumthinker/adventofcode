0d2458b4eb2b130408262ae163744c9227f7041f
Andrey Teplyashin <platinumthinker@gmail.com>
2015-12-08 13:08:32 +0600

    Resolve 8.2

--- day_8/src/resolve.erl
+++ day_8/src/resolve.erl
@@ -4,17 +4,15 @@
 -mode(compile).
 
 main(_Args) ->
-    erlang:display(lists:foldl(fun(A, B) -> B + without_esc(A) end, 0, input())).
+    erlang:display(lists:foldl(fun(A, B) -> B + with_esc(A) end, 0, input())).
 
 input() ->
     {ok, Binary} = file:read_file("input.txt"),
     Data = binary:part(Binary, 0, byte_size(Binary) - 1),
     [ binary_to_list(Str) || Str <- binary:split(Data, [<<"\n">>], [global]) ].
 
-without_esc(L)                        -> length(L) - without_esc(L, - 2).
-without_esc([$\\, $\\ | T], C)        -> without_esc(T, C + 1);
-without_esc([$\\, $" | T], C)         -> without_esc(T, C + 1);
-without_esc([$\\, $x, _A, _B | T], C) -> without_esc(T, C + 1);
-without_esc([$\\, _ | T], C)          -> without_esc(T, C + 1);
-without_esc([_ | T], C)               -> without_esc(T, C + 1);
-without_esc([], C)                    -> C.
+with_esc(L)            -> with_esc(L, 2) - length(L).
+with_esc([$"  | T], C) -> with_esc(T, C + 2);
+with_esc([$\\ | T], C) -> with_esc(T, C + 2);
+with_esc([_ | T], C)   -> with_esc(T, C + 1);
+with_esc([], C)        -> C.
